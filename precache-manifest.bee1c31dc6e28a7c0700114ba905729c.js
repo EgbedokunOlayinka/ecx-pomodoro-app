a163d4d71f661b112c28c915301f2a91053c9455
Egbedokun Olayinka <egbedokunolayinka@gmail.com>
2020-08-03 14:16:01 +0100

    Updates

---
+++ precache-manifest.bee1c31dc6e28a7c0700114ba905729c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482b1ebd00235342ba40ff76d2c88a09",
+    "url": "/ecx-pomodoro-app/index.html"
+  },
+  {
+    "revision": "b4022d34cab1c12c4d35",
+    "url": "/ecx-pomodoro-app/static/css/main.1e28059d.chunk.css"
+  },
+  {
+    "revision": "17ec3cdb99032523f898",
+    "url": "/ecx-pomodoro-app/static/js/2.78476d52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecx-pomodoro-app/static/js/2.78476d52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4022d34cab1c12c4d35",
+    "url": "/ecx-pomodoro-app/static/js/main.7e695fa8.chunk.js"
+  },
+  {
+    "revision": "68d6fc7a17a5fa991451",
+    "url": "/ecx-pomodoro-app/static/js/runtime-main.503c37e2.js"
+  }
+]);
